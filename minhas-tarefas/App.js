d2084287e9898c3c1f9d6ec7d16311f55ffd712f
Gustavo Xavier <gustavo.m.x.dev@gmail.com>
2026-03-05 00:01:47 +0000

    raw: add util and import it

--- minhas-tarefas/App.js
+++ minhas-tarefas/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+import { titulo } from './util';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>{titulo}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ minhas-tarefas/util.js
@@ -0,0 +1,1 @@
+export let titulo = 'Olá, Mundo!';
