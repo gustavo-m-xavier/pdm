05bd8255f3029c07d56ea688f7506a35cc783dfe
Gustavo Xavier <gustavo.m.x.dev@gmail.com>
2026-03-11 23:33:14 +0000

    finish class changes in app js

--- app-atividade-aula-tarefas/App.js
+++ app-atividade-aula-tarefas/App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import { rotulo_btn_cadastro_meta, rotulo_input_meta, rotulo_lista_metas } from './mensagem'
+import { Button, TextInput } from 'react-native-web'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.mainContainer}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%', flex: 1 }}>
+        <View style={{ width: '65%'}}>
+          <TextInput placeholder={rotulo_input_meta} style={styles.inputText} />
+        </View>
+        <View style={{ width: '30%'}}>
+          <Button title={rotulo_btn_cadastro_meta} />
+        </View>
+      </View>
+      <View style={styles.metaContainer}>
+        <Text>{rotulo_lista_metas}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
+    padding: 30,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   },
+  inputText: {
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    padding: 7,
+    borderRadius: 6
+  },
+  metaContainer: {
+    flex: 1
+  }
 });
