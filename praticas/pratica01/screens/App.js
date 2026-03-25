f3300ee517eb51b6e2338c92d89ebc3c09ca4fd3
Gustavo Xavier <gustavo.m.x.dev@gmail.com>
2026-03-25 23:03:44 +0000

    feat: end firts part of pratica01

--- praticas/pratica01/screens/App.js
+++ praticas/pratica01/screens/App.js
@@ -1,13 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons';
 import { StyleSheet, Text, View } from 'react-native';
+import DespesaRecente from './screens/DespesaRecentes';
+import TodasDespesas from './screens/TodasDespesas';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import GerenciarDespesa from './screens/GerenciarDespesa';
+import IconButton from './components/IconButton';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const Tab = createBottomTabNavigator()
+
+  function bottomTabScreen() {
+
+    const navigator = useNavigation()
+
+    return (
+      <Tab.Navigator
+        screenOptions={{headerRight: () => <IconButton icon="add" size={24} onPress={() => {navigator.navigate('GerenciarDespesa')}} />}}>
+        <Tab.Screen name="DespesaRecentes" component={DespesaRecente} 
+          options={{tabBarIcon: ({focused=false, color, size}) => (<Ionicons name="hourglass" size={size} color={color} />),
+          tabBarLabel: 'Recentes',
+          title: 'Despesas Recentes',
+          tabBarLabelStyle: {fontSize: 12}}}/>
+        <Tab.Screen name="TodasDespesas" component={TodasDespesas} 
+          options={{tabBarIcon: ({focused=false, color, size}) => (<Ionicons name="list" size={size} color={color} />),
+          tabBarLabel: 'Todas',
+          title: 'Todas as Despesas',
+          tabBarLabelStyle: {fontSize: 12}}}/>
+      </Tab.Navigator>
+    );
+  }
+
+  const Stack = createNativeStackNavigator()
+
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Despesas" component={bottomTabScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="GerenciarDespesa" component={GerenciarDespesa} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
---
+++ praticas/pratica01/screens/screens/DespesaRecentes.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+function DespesaRecente() {
+    return (
+        <Text>Despesa Recente</Text>
+    )
+}
+
+export default DespesaRecente;
