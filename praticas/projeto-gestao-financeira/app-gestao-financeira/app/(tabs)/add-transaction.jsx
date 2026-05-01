eeb34a7bb1968c0287aeaa5384f450c9bbdba236
Xustavo <gugaxavier2005@gmail.com>
2026-05-01 14:07:39 -0300

    feat: end part 5 of project

--- praticas/projeto-gestao-financeira/app-gestao-financeira/app/(tabs)/add-transaction.jsx
+++ praticas/projeto-gestao-financeira/app-gestao-financeira/app/(tabs)/add-transaction.jsx
@@ -9,11 +9,13 @@ import {
 } from "react-native";
 import { globalStyles } from "../../styles/globalStyles";
 import Button from "../../components/Button";
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import DescriptionInput from "../../components/DescriptionInput";
 import CurrencyInput from "../../components/CurrencyInput";
 import DatePicker from "../../components/DatePicker";
 import CategoryPicker from "../../components/CategoryPicker";
+import { MoneyContext } from "../../contexts/GlobalState";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const initialForm = {
   description: "",
@@ -26,18 +28,29 @@ export default function AddTransactions() {
   const [form, setForm] = useState(initialForm);
   const valueInputRef = useRef();
 
-  const addTransaction = () => {
-    Alert.alert(
-      "Dados Prontos!",
-      `${form.description} | ${form.value} | ${form.date.toLocaleDateString()} | ${form.category}`,
-    );
+  const [transactions, setTransactions] = useContext(MoneyContext);
+
+  const setAsyncStorage = async (data) => {
+    try {
+      await AsyncStorage.setItem("transactions", JSON.stringify(data));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const addTransaction = async () => {
+    const newTransaction = { id: transactions.length + 1, ...form };
+    const updatedTransactions = [...transactions, newTransaction];
+
+    setTransactions(updatedTransactions);
+    setForm(initialForm);
+    await setAsyncStorage(updatedTransactions);
+
+    Alert.alert("Sucesso!", "Transação adicionada com sucesso!");
   };
 
   return (
-    <KeyboardAvoidingView
-      style={globalStyles.screenContainer}
-      behavior="padding"
-    >
+    <KeyboardAvoidingView style={globalStyles.screenContainer}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <ScrollView style={globalStyles.content}>
           <View style={styles.form}>
@@ -62,9 +75,5 @@ export default function AddTransactions() {
 }
 
 const styles = StyleSheet.create({
-  form: {
-    gap: 12,
-    marginBottom: 40,
-    marginTop: 10,
-  },
+  form: { gap: 12, marginBottom: 40, marginTop: 10 },
 });
