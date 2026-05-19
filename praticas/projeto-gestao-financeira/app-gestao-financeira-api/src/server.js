bdc9cdadcbf9661dd5fb7e559c76de59bf340550
Xustavo <gugaxavier2005@gmail.com>
2026-05-19 17:26:31 -0300

    fix: put auth rest in the correct place

--- praticas/projeto-gestao-financeira/app-gestao-financeira-api/src/server.js
+++ praticas/projeto-gestao-financeira/app-gestao-financeira-api/src/server.js
@@ -5,7 +5,7 @@ import { execSync } from "child_process";
 
 import categoriesRouter from "./routes/categories.js";
 import transactionsRouter from "./routes/transactions.js";
-import authRouter from "./routes/auth.js"
+import authRouter from "./routes/auth.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 app.use("/categories", categoriesRouter);
 app.use("/transactions", transactionsRouter);
-app.use("/auth", )
+app.use("/auth", authRouter);
 
 app.use(errorHandler);
 
